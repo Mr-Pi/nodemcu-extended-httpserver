8e913e29e5a24fbb8d931140583ca8d311cc6843
Mr. Pi <github-contact@mr-pi.de>
2016-03-05 19:48:34 +0100

    get responder list

--- httpserver/updateResponders.lua
+++ httpserver/updateResponders.lua
@@ -1,12 +1,22 @@
 -- vim: ts=4 sw=4
 --
---
+-- collects all http responder and add them
 
 require "console"
 
 
 if not httpreq then httpreq={} end
-if not httpreq.responder then httpreq.responder={"httpserver/dynamic","httpserver/static"} end
+if not httpreq.responder then
+	httpreq.responder = {}
+	local fslist = file.list()
+	table.foreach(fslist, function(name, size)
+		if name:match("^httpresponder/") then
+			table.insert(httpreq.responder, name)
+		end
+	end)
+	fslist = nil
+	table.sort(httpreq.responder)
+	collectgarbage()
+end
 
-
-return console.moduleLoaded(...)
+return nil
