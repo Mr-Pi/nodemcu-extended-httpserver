1643fcb38e532fe5c83688e64557e3a0fc2d8b65
Mr. Pi <github-contact@mr-pi.de>
2016-03-05 21:24:51 +0100

    new mem efficient way to compile lua modules

--- httpserver/compile.lua
+++ httpserver/compile.lua
@@ -1,27 +1,22 @@
 -- vim: sw=4 ts=4
 --
--- http module builder
+-- module builder
 
 require "console"
 
+local exclude={}
+exclude["init.lua"] = true
+exclude["httpserver/compile.lua"] = true
 
-local function compileAll()
-	for name, size in pairs(file.list()) do
-		if name:find("\.lua$") and not name:find("^init.lua$") then
-			console.log("compile: "..name)
-			node.compile(name)
-			file.remove(name)
-		end
+for name, size in pairs(file.list()) do
+	if name:find("\.lua$") and not exclude[name] then
+		console.log("compile: "..name)
+		node.compile(name)
+		file.remove(name)
 	end
-	name=nil size=nil
-	collectgarbage()
 end
-compileAll()
+name=nil size=nil exclude=nil
 
+collectgarbage()
 
-local function loaded(module)
-	console.moduleLoaded(module)
-	package.loaded[module]=nil
-	collectgarbage()
-end
-return loaded(...)
+return nil
